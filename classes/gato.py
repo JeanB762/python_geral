e8f8ebde39c128f176cff65a459a81132fcaa39b
Jean Borges <jeanborges946@gmail.com>
2020-06-19 22:53:04 -0300

    classes

---
+++ classes/gato.py
@@ -0,0 +1,11 @@
+class gato:
+  def __init__(self, nome, cor, idade):
+    self.nome = nome
+    self.cor = cor
+    self.idade = idade
+
+mingau = gato('Mingau', 'Branco', 5)
+
+print(mingau.nome)
+print(mingau.cor)
+print(mingau.idade)
