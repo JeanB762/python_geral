315527fefb953aea3680a8bdf28a46dde991b378
Jean Borges <jeanborges946@gmail.com>
2020-06-21 08:59:50 -0300

    configuraçao __sub__, __rsub__, __truediv__ e __rtruediv__

--- objetos/objeto.py
+++ objetos/objeto.py
@@ -14,15 +14,31 @@ class num:
   def __radd__(self, op):
     return self.numero + op
 
+  def __sub__(self, op):
+    return self.numero - op
+
+  def __rsub__(self, op):
+    return self.numero - op
+
   def __mul__(self, op):
     return self.numero * op
   
   def __rmul__(self, op):
     return self.numero * op
 
+  def __truediv__(self, op):
+    return float(self.numero / op)
+
+  def __rtruediv__(self, op):
+    return float(self.numero / op)
+
 n = num(8)
 print(n)
-print(n * 2)  # O metodo __mul__ configura a operação entre a nossa classe * numero
-print(2 * n)  # O metodo __rmul__ configura a operação entre a um numero * nossa classe
-
-## Agora o objeto esta apto a realizar as operações de multiplicação com os inteiros nativos
+print(n + 2)
+print(2 + n)
+print(n - 2)
+print(2 - n)
+print(n * 2)
+print(2 * n)
+print(n / 2)
+print(2 / n)
