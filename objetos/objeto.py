e87859767dbc2cb6cc9117e4727d8c8aadf90fc0
Jean Borges <jeanborges946@gmail.com>
2020-06-21 08:27:12 -0300

    configuraçao __add__ e __radd__

--- objetos/objeto.py
+++ objetos/objeto.py
@@ -8,4 +8,15 @@ class num:
   def __repr__(self):
     return 'Num: {}'.format(self.numero)
 
-print(num(8))   # Implementando o metodo __repr__, agora temos um metodo de formatar a saida
+  def __add__(self, op):
+    return self.numero + op
+
+  def __radd__(self, op):
+    return self.numero + op
+
+n = num(8)
+print(n)
+print(n + 2)  # O metodo __add__ configura a operação entre a nossa classe + numero
+print(2 + n)  # O metodo __radd__ configura a operação entre a um numero + nossa classe
+
+## Agora o objeto esta apto a realizar as operações de adição com os inteiros nativos
