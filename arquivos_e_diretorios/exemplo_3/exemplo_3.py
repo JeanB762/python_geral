4b9347283608d8d5764a8e71d45129285fdd3a7e
Jean Borges <jeanborges946@gmail.com>
2020-06-23 23:32:57 -0300

    listagem completa das pastas e arquivos

--- arquivos_e_diretorios/exemplo_3/exemplo_3.py
+++ arquivos_e_diretorios/exemplo_3/exemplo_3.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 # Criação sequencial das pastas
 for x in range(1, 11):
-  os.mkdir('pasta_{}'.format(x))
+  dir_ = 'pasta_{}'.format(x)
+  if not os.path.exists(dir_):
+    os.mkdir(dir_)
 
 
 # Listagem das pastas
@@ -14,5 +16,11 @@ l = [path for path in os.listdir('.') if os.path.isdir(path)]
 
 # Criação dos arquivos nas respéctivas pastas
 for path in l:
-  Path('{}/arquivo_1.txt'.format(path)).touch()
-  Path('{}/arquivo_2.txt'.format(path)).touch()
+  Path(os.path.join(path, 'arquivo_1.txt')).touch()
+  Path(os.path.join(path, 'arquivo_2.txt')).touch()
+  # Path('{}/arquivo_1.txt'.format(path)).touch()
+  # Path('{}/arquivo_2.txt'.format(path)).touch()
+
+
+for val in os.walk('.'):
+  print(val)
