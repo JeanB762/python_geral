6a36383bb154e51dfc6237c71f7021be40d8ee7e
Jean Borges <jeanborges946@gmail.com>
2020-06-23 23:15:23 -0300

    Criaçao dos arquivos nas pastas

--- arquivos_e_diretorios/exemplo_3/exemplo_3.py
+++ arquivos_e_diretorios/exemplo_3/exemplo_3.py
@@ -3,9 +3,16 @@ import os.path
 import shutil
 from pathlib import Path
 
+
+# Criação sequencial das pastas
 for x in range(1, 11):
   os.mkdir('pasta_{}'.format(x))
 
+
+# Listagem das pastas
 l = [path for path in os.listdir('.') if os.path.isdir(path)]
 
-print(l)
+# Criação dos arquivos nas respéctivas pastas
+for path in l:
+  Path('{}/arquivo_1.txt'.format(path)).touch()
+  Path('{}/arquivo_2.txt'.format(path)).touch()
